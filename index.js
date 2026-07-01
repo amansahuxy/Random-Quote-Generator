ddae56cafad8b54a23cc6ac20abc0e1a3c200414
Aman Sahu <amansahuxy@gmail.com>
2026-07-01 16:04:15 +0530

    Set up async function for API request

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const Quote=document.getElementById("quote");
 
 Button.addEventListener(click,async ()=>{
     try{
-        const resposnse=await fetch("https://dummyjson.com/quotes/random")
+        const resposnse=await fetch("https://dummyjson.com/quotes/random");
+        const data=await response.json();
+        
     }
 })
